a372e8f30635cf56628f4ecb539211cb3e2dbac6
pinulito\oscar.alfaro <oscar.alfaro@corporacionalisa.com>
2026-01-14 10:37:02 -0600

    Envio de notificaciones comentado por mantenimiento

--- controllers/pioapp/visita.controller.js
+++ controllers/pioapp/visita.controller.js
@@ -175,50 +175,51 @@ async function createVisitaEmergencia(req, res) {
 
             const caso = await CasoModel.findByPk(id_caso);
 
-            const usersEmail = await UserModel.findAll({
-                where: {
-                    [Op.or]: [
-                        { id_users: req.user.id_user },
-                        { division: division },
-                        { id_users: caso.userCreatedAt}
-                    ]
-                },
-                attributes: ['email'],
-                raw: true
-            });
+            // const usersEmail = await UserModel.findAll({
+            //     where: {
+            //         [Op.or]: [
+            //             { id_users: req.user.id_user },
+            //             { division: division },
+            //             { id_users: caso.userCreatedAt}
+            //         ]
+            //     },
+            //     attributes: ['email'],
+            //     raw: true
+            // });
 
-            const emailsList = usersEmail.map(u => u.email).filter(Boolean).join(", ");
+            // const emailsList = usersEmail.map(u => u.email).filter(Boolean).join(", ");
 
-            const htmlBody = `
-                <h1>SE HA ASIGNADO LA VISITA ${nuevaVisita.id_visita} PARA LA TIENDA: ${tienda_nombre}</h1>
-                <p>${comentario}</p>
-                <p style='color: red;'>Puedes ver el estado del la visita en: https://pioapp.pinulitogt.com/</p>`;
+            // const htmlBody = `
+            //     <h1>SE HA ASIGNADO LA VISITA ${nuevaVisita.id_visita} PARA LA TIENDA: ${tienda_nombre}</h1>
+            //     <p>${comentario}</p>
+            //     <p style='color: red;'>Puedes ver el estado del la visita en: https://pioapp.pinulitogt.com/</p>
+            //     <p>${emailsList}</>`;
 
-            const basicAuth = Buffer
-                .from(`${process.env.BASIC_NOTI_AUTH_USER}:${process.env.BASIC_NOTI_AUTH_PASS}`)
-                .toString('base64');
+            // const basicAuth = Buffer
+            //     .from(`${process.env.BASIC_NOTI_AUTH_USER}:${process.env.BASIC_NOTI_AUTH_PASS}`)
+            //     .toString('base64');
 
-            const email = await fetch(`https://services.sistemaspinulito.com/notificaciones/mail/send`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${basicAuth}`,
-                },
-                body: JSON.stringify({
-                    emisor: 'PIOAPP',
-                    email_receptor: emailsList,
-                    asunto: `AVISO VISITA ASIGNADA: ${tienda_nombre}`,
-                    data_context: {
-                        body: htmlBody
-                    }
-                })
-            });
+            // const email = await fetch(`https://services.sistemaspinulito.com/notificaciones/mail/send`, {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Authorization': `Basic ${basicAuth}`,
+            //     },
+            //     body: JSON.stringify({
+            //         emisor: 'PIOAPP',
+            //         email_receptor: 'oscar.alfaro@corporacionalisa.com',
+            //         asunto: `AVISO VISITA ASIGNADA: ${tienda_nombre}`,
+            //         data_context: {
+            //             body: htmlBody
+            //         }
+            //     })
+            // });
 
-            const emailNotification = await email.json();
+            // const emailNotification = await email.json();
             
-            if(!email.ok){
-                throw new Error(emailNotification.message);    
-            }
+            // if(!email.ok){
+            //     throw new Error(emailNotification.message);    
+            // }
 
             //Envío de notificaciones al supervisor asignado
             const notification = await fetch(`https://services.sistemaspinulito.com/pioapi/notificaciones/send`, {
